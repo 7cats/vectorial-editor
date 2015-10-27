eaa0da7d2e8e000c4ad6e3d441a58c546ca141fa
razum <acmxca@gmail.com>
2015-10-27 18:08:12 +1000

    Added TViewPort class

--- ufield.pas
+++ ufield.pas
@@ -8,11 +8,73 @@ uses
     Classes, SysUtils;
 
 type
+
     TFloatPoint = record
         x, y: extended;
     end;
 
+    { TViewPort }
+
+    TViewPort = class
+        FCenter: TFloatPoint;
+        FDisplacement, FZoom: extended;
+        FPaintBoxCenterX, FPaintBoxCenterY: double;
+        constructor Create(width, height: double);
+        function WorldToScreen(fpoint: TFloatPoint): TPoint;
+        function ScreenToWorld(point: TPoint): TFloatPoint;
+        procedure AddDisplacement(dispacementX, dispacementY: extended);
+        procedure CalcAndAddDisplacement(oldPoint, newPoint: TPoint);
+        procedure PaintBoxResize(BoxCentreX, BoxCentreY: double);
+    end;
+
+var
+    ViewPort: TViewPort;
+
 implementation
+
+
+{ TViewPort }
+
+function TViewPort.WorldToScreen(fpoint: TFloatPoint): TPoint;
+begin
+    WorldToScreen.X := round((fpoint.x - (FCenter.x - FPaintBoxCenterX)) / FZoom);
+    WorldToScreen.Y := round((fpoint.y - (FCenter.y - FPaintBoxCenterY)) / FZoom);
+end;
+
+function TViewPort.ScreenToWorld(point: TPoint): TFloatPoint;
+begin
+    ScreenToWorld.x := point.x * FZoom + (FCenter.x - FPaintBoxCenterX);
+    ScreenToWorld.y := point.y * FZoom + (FCenter.y - FPaintBoxCenterY);
+end;
+
+constructor TViewPort.Create(width, height: double);
+begin
+    FCenter.x := 0;
+    FCenter.y := 0;
+    FZoom := 1;
+    FPaintBoxCenterY:= height;
+    FPaintBoxCenterX:= width;
+    FDisplacement:= 0;
+end;
+
+procedure TViewPort.AddDisplacement(dispacementX, dispacementY: extended);
+begin
+    FCenter.x += dispacementX;
+    FCenter.y += dispacementY;
+end;
+
+procedure TViewPort.CalcAndAddDisplacement(oldPoint, newPoint: TPoint);
+begin
+    AddDisplacement(newPoint.x - oldPoint.x, newPoint.y - oldPoint.y);
+end;
+
+procedure TViewPort.PaintBoxResize(boxCentreX, boxCentreY: double);
+begin
+    FCenter.x += boxCentreX - FPaintBoxCenterX;
+    FCenter.y += boxCentreY - FPaintBoxCenterY;
+    FPaintBoxCenterX := boxCentreX;
+    FPaintBoxCenterY := boxCentreY;
+end;
 
 end.
 
